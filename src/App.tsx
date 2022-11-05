73fde9625439f8303f9488a189df0161c08e82bb
Thyago Ribeiro <thyagoribeiro608@gmail.com>
2022-11-05 17:49:41 -0400

    Added type Styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+import { ThemeProvider } from "styled-components";
+import { Button } from "components/Button";
+import { defaultTheme } from "styles/themes/default";
+import { GlobalStyle } from "styles/global";
+
 export function App() {
-  return <div>Hello ignite timer</div>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Button />
+      <GlobalStyle />
+    </ThemeProvider>
+  );
 }
---
+++ src/styles/global.ts
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    body{
+        background: #333;
+        color: #fff;
+    }
+
+    button{
+        cursor: pointer;
+    }
+
+    a{
+        text-decoration: none;
+    }
+`;
