23656fed341d6ae8ffcdf3a4011490e9b4a2b2da
Thyago Ribeiro <thyagoribeiro608@gmail.com>
2022-11-17 21:04:09 -0400

    Added Minutes and Seconds in screen

--- src/pages/History/styles.ts
+++ src/pages/History/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const HistoryContainer = styled.div`
   flex: 1;
@@ -9,9 +9,9 @@ export const HistoryContainer = styled.div`
 
   h1 {
     font-size: 1.5rem;
-    color: ${(props) => props.theme['gray-100']};
+    color: ${(props) => props.theme["gray-100"]};
   }
-`
+`;
 
 export const HistoryList = styled.div`
   flex: 1;
@@ -24,10 +24,10 @@ export const HistoryList = styled.div`
     min-width: 600px;
 
     th {
-      background: ${(props) => props.theme['gray-600']};
+      background: ${(props) => props.theme["gray-600"]};
       padding: 1rem;
       text-align: left;
-      color: ${(props) => props.theme['gray-100']};
+      color: ${(props) => props.theme["gray-100"]};
       font-size: 0.875rem;
       line-height: 1.6;
 
@@ -42,8 +42,9 @@ export const HistoryList = styled.div`
       }
     }
     td {
-      background: ${(props) => props.theme['gray-700']};
-      border-top: 4px solid ${(props) => props.theme['gray-800']};
+      background: ${(props) => props.theme["gray-700"]};
+      border-top: 4px solid
+        ${(props) => props.theme["gray-800"]};
       padding: 1rem;
       font-size: 0.875rem;
       line-height: 1.6;
@@ -58,16 +59,16 @@ export const HistoryList = styled.div`
       }
     }
   }
-`
+`;
 
 const STATUS_COLORS = {
-  yellow: 'yellow-500',
-  green: 'green-500',
-  red: 'red-500',
-} as const
+  yellow: "yellow-500",
+  green: "green-500",
+  red: "red-500",
+} as const;
 
 interface StatusProps {
-  statusColor: keyof typeof STATUS_COLORS
+  statusColor: keyof typeof STATUS_COLORS;
 }
 
 export const Status = styled.span<StatusProps>`
@@ -76,13 +77,14 @@ export const Status = styled.span<StatusProps>`
   gap: 0.5rem;
 
   &::before {
-    content: '';
+    content: "";
 
     width: 0.5rem;
     height: 0.5rem;
 
     border-radius: 50%;
 
-    background: ${(props) => props.theme[STATUS_COLORS[props.statusColor]]};
+    background: ${(props) =>
+      props.theme[STATUS_COLORS[props.statusColor]]};
   }
-`
+`;
