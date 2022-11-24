66066cb3af0ff4907a93b46e6924b0c422fc1c82
Thyago Ribeiro <thyagoribeiro608@gmail.com>
2022-11-23 21:24:16 -0400

    Added Cycles in Context

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -15,6 +15,7 @@ interface Cycle {
 }
 
 interface CyclesContextData {
+  cycles: Cycle[]
   activeCycle: Cycle | undefined
   activeCycleId: string | null
   amountSecondsPassed: number
@@ -92,6 +93,7 @@ export function CyclesContextProvider({ children }: CycleContextProviderProps) {
   return (
     <CyclesContext.Provider
       value={{
+        cycles,
         activeCycle,
         activeCycleId,
         amountSecondsPassed,
